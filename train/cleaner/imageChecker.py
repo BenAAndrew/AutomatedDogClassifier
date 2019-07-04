145ce04ff67bf508275008374eff1db4203122bb
BenAAndrew <bandrew01@quba.ac.uk>
2019-07-04 12:36:29 -0700

    updated cleaner

--- train/cleaner/imageChecker.py
+++ train/cleaner/imageChecker.py
@@ -5,35 +5,44 @@ import os
 #min accuracy in %
 minAccuracy = 20
 #max similatenous threads for multithreading
-max_similtaneous_threads = 12
+max_similtaneous_threads = 1
 #text recording to be deleted list 
 toBeDeleted = dict()
+#log of classifications for vertification
+log = list()
 
+#classify log
+logFile = "../log.txt"
 #ToBeDeleted text file path
-toBeDeletedFile = "ToBeDeleted.txt"
+toBeDeletedFile = "../ToBeDeleted.txt"
 #dataSet path
-dataSetPath = "../dataset"
+dataSetPath = "../../dataset"
 #class were checking for
 classToBeChecked = "dog"
+#yolo model
+yoloModel = "yolov3.weights"
+#yolo config
+yoloConfig = "cfg/yolov3.cfg"
 
 def checkImage(folder, image):
-        command = "./darknet detect cfg/yolov3-tiny.cfg yolov3-tiny.weights "+dataSetPath+"/"+folder+"/"+image
-        check = subprocess.check_output(command.split(" "))
+        command = ["./darknet","detect",yoloConfig,yoloModel,dataSetPath+"/"+folder+"/"+image]
+        print(command)
+        check = subprocess.check_output(command)
         outputtedLines = check.decode("utf-8").split("\n")
         del outputtedLines[0]
         del outputtedLines[len(outputtedLines)-1]
+        log.append(folder+"/"+image+": "+str(outputtedLines))
         for line in outputtedLines:
                 if classToBeChecked in line:
                         accuracy = line.split(':')[1]
                         accuracy = int(accuracy[0:len(accuracy)-1])
                         if(accuracy > minAccuracy):
                                 return True
-        print(outputtedLines)
         return False
 
 def checkImagesInFolder(folder):
         filesToDeletedInThisFolder = list()
-        for image in os.listdir("../data/"+folder):
+        for image in os.listdir(dataSetPath+"/"+folder):
                 imageValid = checkImage(folder, image)
                 if not imageValid:
                         filesToDeletedInThisFolder.append(image)
@@ -43,18 +52,20 @@ def writeToBeDeletedTextFile():
         f = open(toBeDeletedFile,"w")
         for folder in toBeDeleted:
                 if len(toBeDeleted[folder]) > 0:
-                        f.write(folder+";\n\n")
+                        #f.write(folder+";\n\n")
                         for image in toBeDeleted[folder]:
                                 f.write(image+"\n")
-                        f.write("\n")
+                        #f.write("\n")
+        f.close()
+
+def writeLog():
+        f = open(logFile,"w") 
+        for msg in log:
+                f.write(msg+"\n")
         f.close()
         
-folders = os.listdir(dataSetPath)
-for i in range(0, len(folders)):
-        while " " in folders[i]:
-                folders[i] = folders[i].replace(" ","$")  
-        while "$" in folders[i]:
-                folders[i] = folders[i].replace("$","\ ")
+folders = os.listdir(dataSetPath)[2:3]
+print(folders)
 
 if __name__ == "__main__":
         with concurrent.futures.ThreadPoolExecutor(max_workers=max_similtaneous_threads) as executor:
@@ -65,3 +76,4 @@ for folder in toBeDeleted:
         print(folder+": "+str(len(toBeDeleted[folder])))
 
 writeToBeDeletedTextFile()
+writeLog()
